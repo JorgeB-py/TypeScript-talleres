13e0a1226e30abcb81dfb49ff33f05fa032dc478
JorgeB-py <j.bustamantep@uniandes.edu.co>
2023-10-22 14:46:58 -0500

    Taller 2

--- main.ts
+++ main.ts
@@ -1,20 +1,65 @@
 import { Serie } from "./serie.js";
 import { series } from "./data.js";
 
-// Path: main.ts
-const seriesTable: HTMLElement = document.getElementById('series')!;
-const seriesAverage: HTMLElement = document.getElementById('average')!;
+let seriesTable: HTMLElement = document.getElementById('series')!;
+let seriesAverage: HTMLElement = document.getElementById('average')!;
+let tablaDatos: HTMLElement = document.getElementById("tabla-datos")!;
+let containerCard: HTMLElement = document.getElementById("container-card")!;
+let serieDetailsCard: HTMLElement | null = null; // Nueva tarjeta para detalles de serie
+
+tablaDatos.addEventListener("click", function (event) {
+    const activeElement = event.target as HTMLElement;
+
+    if (activeElement.tagName == "TD") {
+        const fila = activeElement.parentElement as HTMLTableRowElement;
+        const id = fila.cells[0].textContent;
+
+        if (id !== null) {
+            const serieId = parseInt(id);
+            const selectedSerie = series.find((serie: Serie) => serie.id === serieId);
+
+            if (selectedSerie) {
+                mostrarSerie(selectedSerie);
+            }
+        }
+    }
+});
+
+function mostrarSerie(selectedSerie: Serie) {
+    if (serieDetailsCard) {
+        serieDetailsCard.remove(); // Elimina la tarjeta anterior si existe
+    }
+
+    serieDetailsCard = document.createElement("div");
+    serieDetailsCard.className = "card";
+    serieDetailsCard.style.top = "5px";
+    serieDetailsCard.style.left= "400px"
+    serieDetailsCard.style.width = "18rem";
+    serieDetailsCard.innerHTML = `<img class="card-img-top" src="${selectedSerie.image}" alt="image">
+        <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">${selectedSerie.name}</h5>
+            <p class="card-text">${selectedSerie.description}</p>
+            <p class="card-text">Channel: ${selectedSerie.channel}</p>
+            <p class="card-text">Seasons: ${selectedSerie.seasons}</p>
+            <a href="${selectedSerie.link}" class="btn btn-primary" target="_blank">Learn more</a>
+        </div>
+    `;
+
+    containerCard.innerHTML = ""; // Limpia el contenedor de tarjeta
+    containerCard.appendChild(serieDetailsCard);
+}
+
 
 function renderSeries(series: Serie[]): void {
     series.forEach((serie) => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<th>${serie.id}</th>
-                              <td>${serie.name}</td>
+                              <td style="color:#0000FF">${serie.name}</td>
                                <td>${serie.channel}</td>
                                <td>${serie.seasons}</td>`;
         seriesTable.appendChild(trElement);
-      });
-    
+    });
+
 }
 
 function renderAvarage(series: Serie[]): void {
@@ -23,8 +68,6 @@ function renderAvarage(series: Serie[]): void {
     trElement.innerHTML = `
                            <td>${average}</td>`;
     seriesAverage.appendChild(trElement);
-
-
 }
 
 function calculateAvarage(series: Serie[]): number {
@@ -33,6 +76,10 @@ function calculateAvarage(series: Serie[]): number {
         seasons += serie.seasons;
     }
     return seasons / series.length;
+}
+
+function renderCard(series: Serie[]): void {
+
 }
 
 renderSeries(series);
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,69 @@
 import { series } from "./data.js";
-// Path: main.ts
-var seriesTable = document.getElementById('series');
-var seriesAverage = document.getElementById('average');
+let seriesTable = document.getElementById('series');
+let seriesAverage = document.getElementById('average');
+let tablaDatos = document.getElementById("tabla-datos");
+let containerCard = document.getElementById("container-card");
+let serieDetailsCard = null; // Nueva tarjeta para detalles de serie
+tablaDatos.addEventListener("click", function (event) {
+    const activeElement = event.target;
+    if (activeElement.tagName == "TD") {
+        const fila = activeElement.parentElement;
+        const id = fila.cells[0].textContent;
+        if (id !== null) {
+            const serieId = parseInt(id);
+            const selectedSerie = series.find((serie) => serie.id === serieId);
+            if (selectedSerie) {
+                mostrarSerie(selectedSerie);
+            }
+        }
+    }
+});
+function mostrarSerie(selectedSerie) {
+    if (serieDetailsCard) {
+        serieDetailsCard.remove(); // Elimina la tarjeta anterior si existe
+    }
+    serieDetailsCard = document.createElement("div");
+    serieDetailsCard.className = "card";
+    serieDetailsCard.style.top = "5px";
+    serieDetailsCard.style.left = "400px";
+    serieDetailsCard.style.width = "18rem";
+    serieDetailsCard.innerHTML = `<img class="card-img-top" src="${selectedSerie.image}" alt="image">
+        <div class="card-body">
+            <h5 class="card-title" style="font-weight: bold;">${selectedSerie.name}</h5>
+            <p class="card-text">${selectedSerie.description}</p>
+            <p class="card-text">Channel: ${selectedSerie.channel}</p>
+            <p class="card-text">Seasons: ${selectedSerie.seasons}</p>
+            <a href="${selectedSerie.link}" class="btn btn-primary" target="_blank">Learn more</a>
+        </div>
+    `;
+    containerCard.innerHTML = ""; // Limpia el contenedor de tarjeta
+    containerCard.appendChild(serieDetailsCard);
+}
 function renderSeries(series) {
-    series.forEach(function (serie) {
-        var trElement = document.createElement("tr");
-        trElement.innerHTML = "<th>".concat(serie.id, "</th>\n                              <td>").concat(serie.name, "</td>\n                               <td>").concat(serie.channel, "</td>\n                               <td>").concat(serie.seasons, "</td>");
+    series.forEach((serie) => {
+        let trElement = document.createElement("tr");
+        trElement.innerHTML = `<th>${serie.id}</th>
+                              <td style="color:#0000FF">${serie.name}</td>
+                               <td>${serie.channel}</td>
+                               <td>${serie.seasons}</td>`;
         seriesTable.appendChild(trElement);
     });
 }
 function renderAvarage(series) {
-    var average = calculateAvarage(series);
-    var trElement = document.createElement("tr");
-    trElement.innerHTML = "\n                           <td>".concat(average, "</td>");
+    let average = calculateAvarage(series);
+    let trElement = document.createElement("tr");
+    trElement.innerHTML = `
+                           <td>${average}</td>`;
     seriesAverage.appendChild(trElement);
 }
 function calculateAvarage(series) {
-    var seasons = 0;
-    for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
-        var serie = series_1[_i];
+    let seasons = 0;
+    for (let serie of series) {
         seasons += serie.seasons;
     }
     return seasons / series.length;
+}
+function renderCard(series) {
 }
 renderSeries(series);
 renderAvarage(series);
